cce11279f1157954f6d3c26303a7392e601a6006
SergantDornan <microsoftvis@gmail.com>
2026-01-19 17:44:11 +0300

    правки

--- electr-frontend/src/components/Navbar.tsx
+++ electr-frontend/src/components/Navbar.tsx
@@ -1,9 +1,43 @@
 import { Link } from 'react-router-dom';
-
+import { useState, useEffect } from "react";
 export const AppNavbar = () => {
+
+    const [colorIndex, setColorIndex] = useState(0);
+
+  const colors = [
+    "#FF6B6B", // Красный
+    "#4ECDC4", // Бирюзовый
+    "#45B7D1", // Голубой
+    "#FFA07A", // Лососевый
+    "#98D8C8", // Мятный
+    "#F7DC6F", // Желтый
+    "#BB8FCE", // Фиолетовый
+    "#85C1E2", // Небесно-голубой
+    "#F8B739", // Оранжевый
+    "#52B788"  // Зеленый
+  ];
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+    }, 3000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+
     return (
         <header>
-            <Link to="/" className="logo">electrolysis.com</Link>
+            <Link 
+        to="/" 
+        className="logo" 
+        style={{ 
+          color: colors[colorIndex],
+          transition: "color 1s ease-in-out"
+        }}
+      >
+        electrolysis.com
+      </Link>
             
             <div className="refToMenu">
                 <Link to="/">
@@ -18,3 +52,10 @@ export const AppNavbar = () => {
         </header>
     );
 };
+
+
+
+
+
+
+
